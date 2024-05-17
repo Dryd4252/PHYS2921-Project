eb54275b2652160890a6186882bb89d2f9f52bef
David Ryder <dryd4252@uni.sydney.edu.au>
2024-05-17 11:36:52 +1000

    fixed bugs with crashing when no lifetime or wavelength is given, calculated values are given a negative sign, normalised lifetime is calculated, site symmetry is taken in and order is produced.

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ class erbium_host_crystals():
 		oscilator_strength: float,
 		oscilator_strength_unc: float,
 		refractive_index: float,
-		refractive_index_unc: float
+		refractive_index_unc: float,
+		site_symetry: str,
+		site_symetry_order: int,
+		normalised_lifetime: float,
+		normalised_lifetime_unc: float,
 	):	
 		refractive_index = 2 if refractive_index is None else refractive_index
 		self.refractive_index =  self.set_value(refractive_index, refractive_index_unc)
@@ -43,10 +47,48 @@ class erbium_host_crystals():
 
 		self.lifetime_limit = self.set_value(lifetime_limit, lifetime_limit_unc)
 
+
 		self.ignore_attributes = (
 			"ignore_attributes", 
-			"attribute_requiremnts"
+			"attribute_requiremnts",
+			"site_symetry_dict",
 		)
+
+		self.site_symetry_dict = {
+			"O_h": 6,
+			"D_4h": 5,
+			"T_h": 5,
+			"O": 5,
+			"T_d": 5,
+			"D_6h": 5,
+			"C_4h": 4,
+			"D_4": 4,
+			"D_2d": 4,
+			"C_4v": 4,
+			"D_2h": 4,
+			"T": 4,
+			"D_6": 4,
+			"C_6h": 4,
+			"C_6v": 4,
+			"D_3d": 4,
+			"D_3h": 4,
+			"S_4": 3,
+			"C_4": 3,
+			"D_2": 3,
+			"C_2h": 3,
+			"C_2v": 3,
+			"C_6": 3,
+			"C_3i": 3,
+			"D_3": 3,
+			"C_3v": 3,
+			"C_3h": 3,
+			"C_i": 2,
+			"C_2": 2,
+			"C_s": 2,
+			"C_3": 2,
+			"C_1": 1,
+
+		}
 
 		self.attribute_requiremnts = {
 		"spontaneous_lifetime": [
@@ -60,14 +102,22 @@ class erbium_host_crystals():
 		"optical_lifetime": (("branching_ratio", "spontaneous_lifetime"), self.solve_optical_lifetime),
 		"branching_ratio": (("optical_lifetime", "spontaneous_lifetime"), self.solve_branching_ratio),
 		"oscilator_strength": (("dipole_moment", "wavelength"), self.solve_oscilator_strength),
-		"lifetime_limit": (("optical_lifetime"), self.solve_lifetime_limit)
+		"lifetime_limit": (("optical_lifetime",), self.solve_lifetime_limit)
 		}
 
 		self.get_missing_values()
 
-		self.wavelength *= 1e9 if self.wavelength is not None else None
-		self.optical_lifetime *= 1e3 if self.optical_lifetime is not None else None
-		self.spontaneous_lifetime = self.spontaneous_lifetime * 1e3 if self.spontaneous_lifetime is not None else None
+		self.site_symetry = site_symetry		
+
+		self.site_symetry_order = self.site_symetry_dict[self.site_symetry] if site_symetry != None else None
+
+		if self.optical_lifetime is not None:
+			normalised_lifetime = self.optical_lifetime * self.refractive_index
+			self.normalised_lifetime = normalised_lifetime if normalised_lifetime < 0 else -1 *normalised_lifetime
+
+		self.wavelength = self.wavelength * 1e9 if self.wavelength != None else None
+		self.optical_lifetime = self.optical_lifetime * 1e3 if self.optical_lifetime != None else None
+		self.spontaneous_lifetime = self.spontaneous_lifetime * 1e3 if self.spontaneous_lifetime != None else None
 
 	@staticmethod
 	def set_value(value, unc):
@@ -103,6 +153,10 @@ class erbium_host_crystals():
 		ordered_attributes.append(self.oscilator_strength.s if self.oscilator_strength is not None else None)
 		ordered_attributes.append(self.refractive_index.n if self.refractive_index is not None else None)
 		ordered_attributes.append(self.refractive_index.s if self.refractive_index is not None else None)
+		ordered_attributes.append(self.site_symetry if self.refractive_index is not None else None)
+		ordered_attributes.append(self.site_symetry_order if self.refractive_index is not None else None)
+		ordered_attributes.append(self.normalised_lifetime.n if self.normalised_lifetime.n is not None else None)
+		ordered_attributes.append(self.normalised_lifetime.s if self.normalised_lifetime.s is not None else None)
 		return ordered_attributes
 
 	def print_values(self):
@@ -135,7 +189,7 @@ class erbium_host_crystals():
 	def solve_spontaneous_lifetime_1(self):
 		# T_spon = T_1  / Branching ratio
 
-		self.spontaneous_lifetime = self.optical_lifetime / self.branching_ratio
+		self.spontaneous_lifetime = -1 * (self.optical_lifetime / self.branching_ratio)
 
 	def solve_dipole_moment_1(self):
 		# \mu = (\hbar * e^2 * f) / (2 * m_e * \omega)
@@ -144,7 +198,7 @@ class erbium_host_crystals():
 		numerator = constants.hbar * (constants.e ** 2) * self.oscilator_strength
 		denominator = 2 * constants.m_e * angular_frequency
 
-		self.dipole_moment = numerator / denominator
+		self.dipole_moment = -1 * (numerator / denominator)
 
 	def solve_spontaneous_lifetime_2(self):
 
@@ -152,24 +206,24 @@ class erbium_host_crystals():
 		numerator = 3 * constants.epsilon_0 * constants.c * (self.wavelength ** 2) * constants.hbar
 		denominator = 4 * constants.pi * self.refractive_index * (self.dipole_moment ** 2) * angular_frequency
 		refractive_index_modifier = (2 * (self.refractive_index ** 2) + 1) / (3 * (self.refractive_index ** 2))
-		self.spontaneous_lifetime = (numerator / denominator) * (refractive_index_modifier ** 2)
+		self.spontaneous_lifetime = -1 * ((numerator / denominator) * (refractive_index_modifier ** 2))
 
 	def solve_dipole_moment_2(self):
 		angular_frequency = 2 * constants.pi * constants.c / self.wavelength
 		numerator = 3 * constants.epsilon_0 * constants.c * (self.wavelength ** 2) * constants.hbar
 		denominator = 4 * constants.pi * self.refractive_index * self.spontaneous_lifetime * angular_frequency
 		refractive_index_modifier = (2 * (self.refractive_index ** 2) + 1) / (3 * (self.refractive_index ** 2))
-		self.dipole_moment = ((numerator / denominator) ** 0.5) * refractive_index_modifier
+		self.dipole_moment = -1 * ( ((numerator / denominator) ** 0.5) * refractive_index_modifier )
 
 	def solve_optical_lifetime(self):
 		# T_1 = Branching ratio * T_spon
 
-		self.optical_lifetime = self.branching_ratio * self.spontaneous_lifetime	
+		self.optical_lifetime = -1 * (self.branching_ratio * self.spontaneous_lifetime)	
 
 	def solve_branching_ratio(self):
 		# Branching ratio = T_1 / T_spon
 
-		self.branching_ratio = self.optical_lifetime / self.spontaneous_lifetime
+		self.branching_ratio = -1 * (self.optical_lifetime / self.spontaneous_lifetime)
 
 	def solve_oscilator_strength(self):
 		# f = (2 * \mu * m_e * \omega) / (\hbar * e^2)
@@ -178,12 +232,12 @@ class erbium_host_crystals():
 		numerator = 2 * (self.dipole_moment ** 2) * constants.m_e * angular_frequency
 		denominator = constants.hbar * (constants.e ** 2)
 
-		self.oscilator_strength = numerator / denominator
+		self.oscilator_strength = -1 * (numerator / denominator)
 
 	def solve_lifetime_limit(self):
 		# \gamma_0 = 1 / (2\pi * T_1)
 
-		self.lifetime_limit = 1 / (2 * constants.pi * self.optical_lifetime)
+		self.lifetime_limit = -1 * (1 / (2 * constants.pi * self.optical_lifetime))
 
 
 def main():
@@ -216,15 +270,21 @@ def main():
 	# Y_2SiO_5 = erbium_host_crystals(1536.4776, None, 11.4, None, 0.2, None, None, None, None, None, None, None, None, None, None, None)
 	# Y_2SiO_5.print_values()
 	# print(Y_2SiO_5.get_values())
+
+	#Calculated values will be returned with a negative sign.
+
 								#wavelength   #lifetime   #branching ratio  #dipole moment
 	YVO_4 = erbium_host_crystals(1529.21, None, 3.34, None  , 0.415, None     , 1.00E-32, None
 								#spont_lifetime  #limit_lifetime  #oscilator_strength  refractive_index
-								, None, None     , None, None     , None, None         , None, None)
+								, None, None     , None, None     , None, None         , None, None 
+								#site symetry #site_symmetry_order  #normalised_lifetime
+								, "D_2d" 			, None 				, None , None)
 	YVO_4.print_values()
 	# print(YVO_4.get_values())
 
 
-	#add normalised T_spon value with refractivce index, (n * T_1)
+	#add normalised T_1 value with refractivce index, (n * T_1)
+	
 
 
 if __name__ == "__main__":
